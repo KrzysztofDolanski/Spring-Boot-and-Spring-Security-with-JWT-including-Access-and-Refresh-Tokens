828ee55c6bca598a10380263e71d4d1e1fe8fae7
KrzysztofDolanski <k.dolanski@wp.pl>
2021-07-27 13:01:06 +0200

    Security initialization

--- src/main/java/com/example/userservice/UserserviceApplication.java
+++ src/main/java/com/example/userservice/UserserviceApplication.java
@@ -1,7 +1,17 @@
 package com.example.userservice;
 
+import com.example.userservice.domain.Role;
+import com.example.userservice.domain.User;
+import com.example.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -10,4 +20,33 @@ public class UserserviceApplication {
         SpringApplication.run(UserserviceApplication.class, args);
     }
 
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService){
+        return args -> {
+          userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new User(null, "Krzysztof", "Krzys", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Arnold", "Abasd", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Maciej", "POL", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Stanisław", "stas", "1234", new ArrayList<>()));
+
+            userService.addRoleToUser("Krzys","ROLE_USER");
+            userService.addRoleToUser("Krzys","ROLE_MANAGER");
+            userService.addRoleToUser("Abasd","ROLE_USER");
+            userService.addRoleToUser("POL","ROLE_USER");
+            userService.addRoleToUser("POL","ROLE_ADMIN");
+            userService.addRoleToUser("stas","ROLE_USER");
+            userService.addRoleToUser("stas", "ROLE_SUPER_ADMIN");
+
+        };
+    }
 }
--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,19 +6,42 @@ import com.example.userservice.repository.RoleRepo;
 import com.example.userservice.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            log.error("User not found");
+            throw new UsernameNotFoundException("User not found");
+        } else {
+            log.info("User found " + username);
+        }
+        Collection<SimpleGrantedAuthority> authirities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authirities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authirities);
+    }
 
     @Override
     public User saveUser(User user) {
